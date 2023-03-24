e73e87dd71fb3feb9dc81b9bf4ac5a2c261c72b3
Junaid Bin Jaman <me@junaidbinjaman.com>
2023-03-24 15:42:02 +0600

    a

---
+++ longestCommonPrefix.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Solution {
+
+  /**
+   * @param String[] $strs
+   * @return String
+   */
+  function longestCommonPrefix($strs) {
+    $result = $strs[0];
+    for ($i = 1; $i < count($strs); $i++) {
+      $str = $strs[i];
+      while (substr($str, 0, strlen($result)) != $result && strlen($result) > 0) {
+        $result = substr(0, strlen($result) - 1);
+      }
+    }
+    return $result;
+  }
+}
+
+// ["flower","flow","flight"]
