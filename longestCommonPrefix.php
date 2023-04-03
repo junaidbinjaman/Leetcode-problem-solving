5d6f2af1f8b255a731bdb39f7a2ac631d6b6c5d9
Junaid Bin Jaman <me@junaidbinjaman.com>
2023-04-03 09:05:38 +0600

    solved conflict

--- longestCommonPrefix.php
+++ longestCommonPrefix.php
@@ -1,8 +1,5 @@
-<?php 
-<<<<<<< HEAD
+<?php
 
-=======
->>>>>>> master
 class Solution {
 
   /**
@@ -12,25 +9,13 @@ class Solution {
   function longestCommonPrefix($strs) {
     $result = $strs[0];
     for ($i = 1; $i < count($strs); $i++) {
-<<<<<<< HEAD
       $str = $strs[i];
       while (substr($str, 0, strlen($result)) != $result && strlen($result) > 0) {
         $result = substr(0, strlen($result) - 1);
-=======
-      $str = $strs[$i];
-      while (substr($str, 0, strlen($result)) != $result && strlen($result) > 0) {
-        $result = substr($result, 0, strlen($result) - 1);
->>>>>>> master
       }
     }
     return $result;
   }
 }
 
-<<<<<<< HEAD
 // ["flower","flow","flight"]
-=======
-$solution = new Solution;
-
-echo $solution->longestCommonPrefix(['Ju', 'Ju', 'Jumana',]) . "\n";
->>>>>>> master
